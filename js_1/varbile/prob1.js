1695b71c6dbe90d0e2db12581a529c9d34f32664
Sachin230498 <sachinpathe230498@gmail.com>
2025-06-18 14:55:30 +0530

    js  -1 start

---
+++ js_1/varbile/prob1.js
@@ -0,0 +1,68 @@
+// varible  - thing that vary the value
+
+// varible is kind of box or container where you can store data
+
+//  let var const
+
+// Data type
+
+// String -  "Hello" ,  'How are you' , "123466"  
+// number -  1,55,789
+// boolean - true/false
+
+
+let name = "Rahul" ; 
+let Name = "kumar"
+
+let num = 45;  
+
+// let myname = "Sachin"
+
+// let my name 
+
+
+// let name
+
+// Rules for varible naming
+
+// Case Sensitivity: Variable names are case-sensitive, meaning "name" and "Name" are considered different variables. 
+// No Spaces: Variable names cannot contain spaces. 
+// Start with a Letter or Underscore: Variable names must begin with a letter or an underscore ( _ ). 
+// Allowed Characters: Variable names can include letters, numbers, and underscores. 
+// No Reserved Words: Keywords used by the programming language (like if, for, class) cannot be used as variable names. 
+// Descriptive Names: Choose names that accurately reflect the variable's purpose. 
+// Pronounceable Names: Variable names should be easy to read and pronounce, aiding in discussions and debugging. 
+
+
+
+
+
+
+
+
+// for comment - ctrl + /
+
+
+
+
+
+let a = 123;
+let b = "Akshay";
+let c = true;
+
+console.log(a)   // 123
+
+console.log(b)  
+
+console.log(c)
+
+
+
+
+//  ()  = paranthesis
+//  []  = square bracket
+//  {}  = curly braces
+//  ;   = semi colon
+//  :   = colon
+//  ""  = double quetes
+//  ` ` = backtic
---
+++ js_1/varbile/prob2.js
@@ -0,0 +1,14 @@
+let greet //declaration
+
+greet = 45; //assigning 
+
+// console.log(typeof greet);
+
+
+let x = "how are you"; 
+
+// console.log(typeof x);
+
+
+let y = "hello";  //intilization
+// console.log(y)
